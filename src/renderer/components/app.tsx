39e54b7f138dbb68ec10db62e6da65c3d951c7d0
uhyo <uhyo@uhy.ooo>
2017-05-05 15:50:23 +0900

    left-right ui

--- src/renderer/components/app.tsx
+++ src/renderer/components/app.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 
 import TreeGroup from '../containers/tree-group';
+import Images from '../components/images';
 
 import '../css/root.css';
 
+import * as styles from '../css/app.css';
+
 export default class App extends React.Component<{}, {}>{
     render(){
-        return <TreeGroup/>;
+        return <div className={styles.wrapper}>
+            <div className={styles.treeGroup}>
+                <TreeGroup/>
+            </div>
+            <div className={styles.images}>
+                <Images/>
+            </div>
+        </div>;
     }
 }
---
+++ src/renderer/components/images.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export interface IPropImages{
+}
+export default class Images extends React.Component<IPropImages, {}>{
+    render(){
+        return <div>
+            Foo!
+        </div>;
+    }
+}
