d29bab75ef0fba9ea6fe07cd86638dce94b3780a
uhyo <uhyo@uhy.ooo>
2017-06-25 01:51:46 +0900

    some window features

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,10 +10,15 @@ import {
 import * as path from 'path';
 import * as url from 'url';
 
+// load theme for background
 let mainWindow: Electron.BrowserWindow | null;
 
 function createWindow(){
-    mainWindow = new BrowserWindow();
+    mainWindow = new BrowserWindow({
+        // VERY AD-HOC
+        backgroundColor: 'hsl(217, 26%, 35%)',
+        show: false,
+    });
 
     const u = url.format({
         pathname: path.resolve(__dirname, '..', '..', 'view', 'index.html'),
@@ -24,6 +29,10 @@ function createWindow(){
 
     mainWindow.on('closed', ()=>{
         mainWindow = null;
+    });
+    const m = mainWindow;
+    mainWindow.once('ready-to-show', ()=>{
+        m.show();
     });
 }
 
